c7ae3930566043521fcd82cce1df9672ec7f73f5
Minora <MinoraTyan@yandex.tu>
2023-10-02 14:56:05 +0300

    38-55

--- DZ_Task_038-55.py
+++ DZ_Task_038-55.py
@@ -2,45 +2,84 @@
 Пользователь также может ввести имя или фамилию, и Вы должны реализовать функционал
 для изменения и удаления данных."""
 
-phonebook = {}
+phonedirectory = {}
 
 def add_contact(name, phone):
-    """Добавление контакта в телефонную книгу"""
-    phonebook[name] = phone
-    print(f"Контакт {name} добавлен в телефонную книгу с номером {phone}.")
+    #Добавление контакта в телефонную книгу
+    phonedirectory[name] = phone
+    update_file()
 
 def update_contact(name, new_phone):
-    """Обновление номера телефона для существующего контакта"""
-    if name in phonebook:
-        phonebook[name] = new_phone
-        print(f"Номер телефона для {name} обновлен: {new_phone}.")
+    #Обновление номера телефона для существующего контакта
+    if name in phonedirectory:
+        phonedirectory[name] = new_phone
+        update_file()
     else:
         print(f"{name} не найден в телефонной книге.")
 
 def delete_contact(name):
-    """Удаление контакта из телефонной книги"""
-    if name in phonebook:
-        del phonebook[name]
-        print(f"Контакт {name} удален из телефонной книги.")
+    #Удаление контакта из телефонной книги
+    if name in phonedirectory:
+        del phonedirectory[name]
+        update_file()
     else:
         print(f"{name} не найден в телефонной книге.")
 
-def print_phonebook():
-    """Печать содержимого телефонной книги"""
-    if phonebook:
-        print("Телефонная книга:")
-        for name, phone in phonebook.items():
+def print_phonedirectory():
+    #Печать содержимого телефонной книги
+    if phonedirectory:
+        print("Телефонная книга: \n")
+        for name, phone in phonedirectory.items():
             print(f"{name}: {phone}")
     else:
         print("Телефонная книга пуста.")
 
-# Пример использования функций:
-add_contact("Иван Иванов", "123-456-789")
-add_contact("Петр Петров", "987-654-321")
+def update_file():
+    #Запись данных в файл
+    with open("phonedirectory.txt", "w") as file:
+        for name, phone in phonedirectory.items():
+            file.write(f"{name}: {phone}\n")
 
-print_phonebook()
+def load_data_from_file():
+    #Чтение данных из файла
+    try:
+        with open("phonedirectory.txt", "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                name, phone = line.strip().split(": ")
+                phonedirectory[name] = phone
+    except FileNotFoundError:
+        pass  # Ignore if the file doesn't exist
 
-update_contact("Иван Иванов", "111-222-333")
-delete_contact("Петр Петров")
+#Загрузка данных из файла при запуске программы
+load_data_from_file()
 
-print_phonebook()
+#Добавление одного контакта
+print("Добавление контакта:")
+name = input("Фамилия Имя Отчество: ")
+phone = input("Номер телефона: ")
+add_contact(name, phone)
+
+#Меню с функциями
+while True:
+    print("\nМеню:")
+    print("1. Печать телефонной книги")
+    print("2. Обновление номера телефона")
+    print("3. Удаление контакта")
+    print("4. Выход")
+    
+    choice = input("Выберите действие (1-4):\n \n")
+
+    if choice == '1':
+        print_phonedirectory()
+    elif choice == '2':
+        name = input("Введите имя контакта для обновления номера: ")
+        new_phone = input("Введите новый номер телефона: ")
+        update_contact(name, new_phone)
+    elif choice == '3':
+        name = input("Введите имя контакта, которого хотите удалить: ")
+        delete_contact(name)
+    elif choice == '4':
+        break
+    else:
+        print("Такого действия не существует! Выберите действие (1-4): ")
